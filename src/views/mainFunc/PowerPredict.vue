788862ffcc7a0072bd9b800aa95b3e056f7240ad
= <=>
2023-07-07 20:20:41 +0800

    修改图标样式

--- src/views/mainFunc/PowerPredict.vue
+++ src/views/mainFunc/PowerPredict.vue
@@ -41,7 +41,10 @@
     <div class="contentArea" style="margin-top: 6px;height: 600px;">
       <!-- 这里是函数图像…… -->
       <!-- 函数图像demo -->
-      <LineChart></LineChart>
+      <!-- <LineChart></LineChart> -->
+      <div style="height: 100%">
+        <div id="container" style="height: 100%"></div>
+      </div>
     </div>
 
     <!-- 数据说明表格 -->
@@ -74,12 +77,15 @@
 
 <script>
 import Tabinfor from "@/components/TabInfor";
-import LineChart from "@/components/LineChart";
+// import LineChart from "@/components/LineChart";
+// 导入echart库
+import * as echarts from 'echarts';
+
 export default {
   name: "PowerPredict",
   components: {
     Tabinfor,
-    LineChart
+    // LineChart
   },
   data() {
     return {
@@ -157,8 +163,86 @@ export default {
       // 导出分析数据逻辑
       console.log('导出分析数据');
     }
+  },
+
+  mounted() {
+    var dom = document.getElementById('container');
+    var myChart = echarts.init(dom, null, {
+      renderer: 'canvas',
+      useDirtyRect: false
+    });
+    
+    let base = +new Date(1988, 9, 3);
+    let oneDay = 24 * 3600 * 1000;
+    
+    let data = [[base, Math.random() * 300]];
+    
+    for (let i = 1; i < 20000; i++) {
+      let now = new Date((base += oneDay));
+      data.push([+now, Math.round((Math.random() - 0.5) * 20 + data[i - 1][1])]);
+    }
+    
+    let option = {
+      tooltip: {
+        trigger: 'axis',
+        position: function(pt) {
+          return [pt[0], '10%'];
+        }
+      },
+      title: {
+        left: 'center',
+        text: 'Large Ara Chart'
+      },
+      toolbox: {
+        feature: {
+          dataZoom: {
+            yAxisIndex: 'none'
+          },
+          restore: {},
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'time',
+        boundaryGap: false
+      },
+      yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%']
+      },
+      dataZoom: [
+        {
+          type: 'inside',
+          start: 0,
+          end: 20
+        },
+        {
+          start: 0,
+          end: 20
+        }
+      ],
+      series: [
+        {
+          name: 'Fake Data',
+          type: 'line',
+          smooth: true,
+          symbol: 'none',
+          areaStyle: {},
+          data: data
+        }
+      ]
+    };
+    
+    if (option && typeof option === 'object') {
+      myChart.setOption(option);
+    }
+    
+    window.addEventListener('resize', myChart.resize);
   }
+
 }
+
+
 
 </script>
 <style lang="less" scoped>
